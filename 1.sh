3a6814e5463f37f18515d0e6faeff3b329e730e6
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2019-07-13 18:35:05 +0900

    first commit

---
+++ 1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# VCCW をダウンロード
+git clone https://github.com/vccw-team/vccw.git
+
+# site.yml を開いて、編集する
+cp vccw/provision/default.yml vccw/site.yml
+open vccw/site.yml
---
+++ 3-theme.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+source parse_yaml.sh
+eval $(parse_yaml site.yml "conf_")
+
+# テーマ ディレクトリ
+DIR=../$conf_hostname/wordpress/wp-content/themes/$conf_hostname
+
+# github reository
+REPOSITORY=wp-theme-dev-env
+
+# src ディレクトリ作成
+mkdir -p $DIR/resources/src/scss $DIR/resources/src/js
+
+# wp-theme-dev-env をインストール
+cd $DIR
+git clone git@github.com:shunk76/$REPOSITORY.git
+cp $REPOSITORY/* ./
+rm -rf $REPOSITORY
+
+# site.yml を削除
+rm -f site.yml
+
+ncu -u
+npm i
